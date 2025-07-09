5ffdebbb3c9ed84ceb0c9f637a644c78e2a59462
jserrano <jserrano@procesos-eficientes.com>
2025-07-09 18:20:16 -0500

    Se cambio el dotenv del archivo

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
-dotenv.config({path:'.env'});
+dotenv.config();
 
 const db=new Sequelize(process.env.BD_NOMBRE,process.env.BD_USUARIO,process.env.BD_PASS,{
     host:process.env.BD_HOST,
