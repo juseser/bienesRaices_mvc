f2f74f6be966353598685d6570ca84d1ce0b99b6
jserrano <jserrano@procesos-eficientes.com>
2025-07-09 18:40:23 -0500

    Se cambio el dotenv del archivo

--- config/db.js
+++ config/db.js
@@ -2,19 +2,28 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db=new Sequelize(process.env.BD_NOMBRE,process.env.BD_USUARIO,process.env.BD_PASS,{
-    host:process.env.BD_HOST,
-    port:process.env.BD_PORT,
-    dialect:'mysql',
-    define:{
-        timestamps:true
+const db = new Sequelize(
+  process.env.BD_NOMBRE,
+  process.env.BD_USUARIO,
+  process.env.BD_PASS,
+  {
+    host: process.env.BD_HOST,
+    port: process.env.BD_PORT ? Number(process.env.BD_PORT) : 3306, // fallback por si falta
+    dialect: 'mysql',
+    define: {
+      timestamps: true,
     },
-    pool:{
-        max:5,
-        min:0,
-        acquire:30000,
-        idle:10000
-    }
-});
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    dialectOptions: {
+      connectTimeout: 10000, // útil para producción
+    },
+    logging: process.env.NODE_ENV === 'development' ? console.log : false
+  }
+);
 
 export default db;
